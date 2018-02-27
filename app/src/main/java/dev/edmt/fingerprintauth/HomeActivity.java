d929b9614b587cecf6bb058ebec848baf889c0c8
Cesar Rivas <csrivas24@gmail.com>
2018-02-26 23:58:38 -0600

    Initial commit

---
+++ app/src/main/java/dev/edmt/fingerprintauth/HomeActivity.java
@@ -0,0 +1,40 @@
+package dev.edmt.fingerprintauth;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+public class HomeActivity extends AppCompatActivity {
+FloatingActionButton fab;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
+ //       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+  //      fab.setOnClickListener(new View.OnClickListener() {
+  //          @Override
+ //           public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                fab=(FloatingActionButton)findViewById(R.id.fab);
+                fab.setOnClickListener( new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent click = new Intent(HomeActivity.this, Main2Activity.class);
+                        startActivity(click);
+                    }
+                });
+
+
+    }
+
+}
